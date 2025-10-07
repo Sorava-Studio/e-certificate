bb8f93caa9b2be161013ef41413d03ac83cf9823
Jones Cian <jonescian@Joness-MacBook-Air.local>
2025-10-07 09:07:41 +0200

    🎉Setup(DEV-11): initialise project basics needs.

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="font-bold text-4xl">Welcome to EMERA Certificate</h1>
+      <Button>Get Started</Button>
+    </main>
+  );
+}
